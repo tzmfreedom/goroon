b8401f8a3413df9dfb28c0d26aacd27d47b8509f
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-09-11 16:57:20 +0900

    Add test code

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/tzmfreedom/goroon"
 )
@@ -53,4 +55,72 @@ func TestGetSession(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestBeginningOfDay(t *testing.T) {
+	now := time.Now()
+	day := beginningOfDay(now)
+	if now.Year() != day.Year() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Year(), day.Year())
+	}
+	if now.Month() != day.Month() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Month(), day.Month())
+	}
+	if now.Day() != day.Day() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Day(), day.Day())
+	}
+	if day.Hour() != 0 {
+		t.Fatalf("failed. expected = 0, actual = %v", day.Hour())
+	}
+	if day.Minute() != 0 {
+		t.Fatalf("failed. expected = 0, actual = %v", day.Minute())
+	}
+	if day.Second() != 0 {
+		t.Fatalf("failed. expected = 0, actual = %v", day.Second())
+	}
+	if day.Nanosecond() != 0 {
+		t.Fatalf("failed. expected = 0, actual = %v", day.Nanosecond())
+	}
+	if now.Location() != day.Location() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Location(), day.Location())
+	}
+}
+
+func TestEndOfDay(t *testing.T) {
+	now := time.Now()
+	day := endOfDay(now)
+	if now.Year() != day.Year() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Year(), day.Year())
+	}
+	if now.Month() != day.Month() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Month(), day.Month())
+	}
+	if now.Day() != day.Day() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Day(), day.Day())
+	}
+	if day.Hour() != 23 {
+		t.Fatalf("failed. expected = 23, actual = %v", day.Hour())
+	}
+	if day.Minute() != 59 {
+		t.Fatalf("failed. expected = 59, actual = %v", day.Minute())
+	}
+	if day.Second() != 59 {
+		t.Fatalf("failed. expected = 59, actual = %v", day.Second())
+	}
+	if day.Nanosecond() != 999999 {
+		t.Fatalf("failed. expected = 999999, actual = %v", day.Nanosecond())
+	}
+	if now.Location() != day.Location() {
+		t.Fatalf("failed. expected = %v, actual = %v", now.Location(), day.Location())
+	}
+}
+
+func TestFormatDate(t *testing.T) {
+	now := time.Now()
+	actual := formatDate(goroon.XmlDate{now})
+	expected := fmt.Sprintf("%04d-%02d-%02d", now.Year(), now.Month(), now.Day())
+	if expected != actual {
+		t.Fatalf("failed. expected = %v, actual = %v", expected, actual)
+	}
+
 }
